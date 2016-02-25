9a3d27bc774b96c1db681932c3c5a771985ffa20
tcourtai <t.courtain@gmail.com>
2016-02-25 14:39:27 -0600

    thymeleaf added

--- dev/src/main/java/hello/GreetingController.java
+++ dev/src/main/java/hello/GreetingController.java
@@ -1,15 +1,23 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GreetingController {
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/")
+	public String index(Model model){
+		model.addAttribute("message", "Hello !");
+		return "hello";
+	}
 	
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
